138c3f5b5d73e798b3c3ef44772ca7f94247edfd
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-07-19 20:40:19 +0900

    configjson

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 
 import SignIn from "./SignIn";
 import Main from "./Main";
+import config from "../config.json";
 
 export default () => {
   const [name, setName] = useState('');
 
-  if (name === '')
+  if (name === '' && config.signInEnabled == true)
     return <SignIn setName={setName} />;
   else
     return <Main name={name} />;
