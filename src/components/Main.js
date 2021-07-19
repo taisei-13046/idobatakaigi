8c2b6d1fd7dc6363f2e6612bd96e402d8b47e1c9
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-07-19 21:11:50 +0900

    apply-grid

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
 
-const Main = () => {
+const useStyles = makeStyles({
+	root: {
+		display: "grid",
+		height: "100vh",
+		gridTemplateRows: "1fr auto",
+	},
+});
+
+const Main = (props) => {
+	const { name } = props;
+	const classes = useStyles();
 	return (
-		<div>Main</div>
+		<div className={classes.root}>
+			<MessageList />
+			<MessageInputField name={name} />
+		</div>
 	)
 }
 
