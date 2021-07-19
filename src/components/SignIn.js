1580867431fc45a7600cda425a8aa1abca414bf3
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-07-19 12:21:18 +0900

    setName by clicl

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -47,6 +42,11 @@ const useStyles = makeStyles((theme) => ({
 export default function SignIn(props) {
   const { setName } = props;
   const classes = useStyles();
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  useEffect(() => {
+      setDisabled(string === '');
+  }, [string]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -65,6 +65,7 @@ export default function SignIn(props) {
             label="ニックネーム"
             name="name"
             autoFocus
+			onChange={(e) => setString(e.target.value)}
           />
           <Button
             type="submit"
@@ -72,6 +73,7 @@ export default function SignIn(props) {
             variant="contained"
             color="primary"
             className={classes.submit}
+			disabled={disabled}
           >
             はじめる
           </Button>
