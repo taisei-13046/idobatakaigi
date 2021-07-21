ef621c080aac85e9bfbffc5fb164910623f053ca
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-07-21 11:07:42 +0900

    edit

--- src/firebase.js
+++ src/firebase.js
@@ -11,9 +11,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = (props) => {
 	const { name, text } = props;
-	messageRef.push({ name, text })
+	messagesRef.push({ name, text })
 }
