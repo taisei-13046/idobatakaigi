055c6ca06e583d7764d3ecde5f0c65ea78d9cba2
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-07-21 11:45:17 +0900

    create-env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,21 @@
 import firebase from "firebase";
 
+const {
+	REACT_APP_FIREBASE_API_KEY,
+	REACT_APP_FIREBASE_AUTH_DOMAIN,
+	REACT_APP_FIREBASE_PROJECT_ID,
+	REACT_APP_FIREBASE_STORAGE_BUCKET,
+	REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-	apiKey: "AIzaSyBXUJD39X7bVGVDz306df6g8fR9bluqi7s",
-	authDomain: "idobatakaigi-with-ytaisei.firebaseapp.com",
-	projectId: "idobatakaigi-with-ytaisei",
-	storageBucket: "idobatakaigi-with-ytaisei.appspot.com",
-	messagingSenderId: "931815181972",
-	appId: "1:931815181972:web:390708b4100087456f2e81"
+	apiKey: REACT_APP_FIREBASE_API_KEY,
+	authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
